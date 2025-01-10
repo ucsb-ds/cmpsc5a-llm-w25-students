49741d1a8c869495d91b71c86230ba64805dcf64
Rutvik Jha <rutvikjha@169-231-57-213.wireless.ucsb.edu>
2025-01-10 01:22:06 -0800

    added lab01/wkst1/wkst2

---
+++ worksheets/worksheet2/tests/q8_4.py
@@ -0,0 +1,3 @@
+OK_FORMAT = True
+
+test = {'name': 'q8_4', 'points': None, 'suites': [{'cases': [{'code': '>>> gradescope == 935786\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
